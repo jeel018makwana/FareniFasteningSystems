0889bf573efae22d5d6cc1857392289b9a2a3e18
Jeel Makwana <jeelmakwana018@gmail.com>
2026-09-15 22:10:20 +0530

    'update'

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,5 @@
+import LoginForm from "../components/LoginForm";
+
 export default function Login() {
-  return (
-    <div className="min-h-screen bg-slate-950 flex items-center justify-center">
-      <h1 className="text-5xl font-bold text-white">
-        Login
-      </h1>
-    </div>
-  );
+  return <LoginForm />;
 }

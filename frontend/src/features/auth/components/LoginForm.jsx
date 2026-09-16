0a465d1997e0cb3250de178fd86cab86db95a748
Jeel Makwana <jeelmakwana018@gmail.com>
2026-09-16 18:44:11 +0530

    Update logo

--- frontend/src/features/auth/components/LoginForm.jsx
+++ frontend/src/features/auth/components/LoginForm.jsx
@@ -121,8 +121,8 @@ export default function LoginForm() {
           <div>
 
             <div className="flex items-center gap-3">
-              
-              <img src="/images/logo.png" alt="Fareni Logo" className="w-full max-w-xl object-contain" />
+
+              <img src="/images/logo.png" alt="Fareni Logo" className="w-full max-w-l object-contain" />
 
               <div>
                 <h1 className="text-3xl font-black tracking-[0.12em] text-white">
